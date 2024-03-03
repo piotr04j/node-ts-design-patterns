fea75c6ee86e341cf727954778e940560bdaec2b
piotr04j <darricck04@gmail.com>
2024-03-03 10:51:26 +0100

    added some rules for eslint and added some command in package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
-      "@typescript-eslint/explicit-function-return-type": "off"
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     }
 }
