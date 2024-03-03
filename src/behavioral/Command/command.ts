dce0bc481b7c5f8d4a75d8fb527b6bb49b25446a
piotr04j <darricck04@gmail.com>
2024-03-03 10:51:48 +0100

    added Command design pattern

---
+++ src/behavioral/Command/command.ts
@@ -0,0 +1,41 @@
+interface Command {
+  execute: () => void
+}
+
+class SaveCommand implements Command {
+  private _receiver: HandleRequest
+  private _payload: string
+
+  constructor(receiver: HandleRequest, payload: string) {
+    this._receiver = receiver
+    this._payload = payload
+  }
+
+  execute() {
+    this._receiver.handle(this._payload)
+  }
+}
+
+class Button {
+  private _saveCommand!: Command
+
+  setSave(command: Command) {
+    this._saveCommand = command
+  }
+
+  onClick() {
+    this._saveCommand.execute()
+  }
+}
+
+class HandleRequest {
+  handle(payload: string) {
+    console.log('Request with payload: ' + payload + ' was handled!')
+  }
+}
+
+const receiver = new HandleRequest()
+const command = new SaveCommand(receiver, '"save this"')
+const invoker = new Button()
+invoker.setSave(command)
+invoker.onClick()
